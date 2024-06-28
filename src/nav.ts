9139fcd659aa550a9e7007bda41de03fc27f93f1
leakedweights <vkristof20@protonmail.com>
2024-06-28 18:18:35 +0200

    temporarily disabled nav

--- src/nav.ts
+++ src/nav.ts
@@ -1,9 +1,9 @@
 export function renderNav(): string {
     return `
-        <nav>
-            <a class="nav-item" href="/">Home</a>
-            <span class="nav-separator">/</span>
-            <a class="nav-item" href="/blog">Blog</a>
-        </nav>
+        // <nav>
+        //     <a class="nav-item" href="/">Home</a>
+        //     <span class="nav-separator">/</span>
+        //     <a class="nav-item" href="/blog">Blog</a>
+        // </nav>
     `
 }
