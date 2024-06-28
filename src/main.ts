1d6319e08371717bb87c28d05f28fa1e806d9693
leakedweights <vkristof20@protonmail.com>
2024-06-28 22:04:25 +0200

    readded katex and nav

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import './style.css';
 import { renderHome, setupCaptcha } from './pages/home';
 import { renderBlog } from './pages/blog';
+import katex from 'katex';
+import 'katex/dist/katex.min.css';
+
+function renderLaTeX() {
+  const elements = document.querySelectorAll('.latex');
+  elements.forEach(element => {
+    katex.render(element.textContent || '', element as HTMLElement, {
+      throwOnError: false
+    });
+  });
+}
 
 function router(): void {
   const path = window.location.pathname;
@@ -14,6 +25,7 @@ function router(): void {
       break;
     case '/blog':
       app.innerHTML = renderBlog();
+      renderLaTeX();
       break;
     default:
       app.innerHTML = `<div><h1>404 Not Found</h1></div>`;
