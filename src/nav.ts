62fc66932e3165a73b0f53a8eceea03adb6f4263
leakedweights <vkristof20@protonmail.com>
2024-07-14 13:04:16 +0200

    fixed commenting in nav

--- src/nav.ts
+++ src/nav.ts
@@ -1,11 +1,12 @@
 export function renderNav(): string {
     return `
-        // <nav>
-        //     <a class="nav-item" href="/">Home</a>
-        //     <span class="nav-separator">/</span>
-        //     <a class="nav-item" href="/foundations">Map of Foundations</a>
-        //     <span class="nav-separator">/</span>
-        //     <a class="nav-item" href="/blog">Blog</a>
-        // </nav>
+        <nav>
+        </nav>
     `
+
+    // <a class="nav-item" href="/">Home</a>
+    // <span class="nav-separator">/</span>
+    // <a class="nav-item" href="/foundations">Map of Foundations</a>
+    // <span class="nav-separator">/</span>
+    // <a class="nav-item" href="/blog">Blog</a>
 }
